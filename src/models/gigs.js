8068e6fe9f3886412b70edf152cb2c3199139d84
Vedant005 <vedantkanekar909@gmail.com>
2025-03-16 21:05:35 +0530

    feat: update gigs schema with additional fields and integrate mongoose-aggregate-paginate

--- src/models/gigs.js
+++ src/models/gigs.js
@@ -1,20 +1,84 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const GigSchema = new mongoose.Schema({
-  title: String,
-  type: String,
-  amount_amount: Number,
-  hourly_rate: Number,
-  duration: String,
-  engagement: String,
-  occupations_category_pref_label: String,
-  occupations_oservice_pref_label: String,
-  client_total_reviews: Number,
-  client_total_spent: Number,
-  published_on: { type: Date, default: Date.now },
-  tier: String,
-  total_freelancers_to_hire: Number,
-  client_location_country: String,
-});
+const gigsSchema = new mongoose.Schema(
+  {
+    connect_price: {
+      type: Number,
+    },
+    created_on: {
+      type: Date,
+    },
+    tz_date: {
+      type: Date,
+    },
+    duration: {
+      type: String,
+    },
+    engagement: {
+      type: String,
+    },
+    freelancers_to_hire: {
+      type: Number,
+    },
+    amount_amount: {
+      type: Number,
+    },
+    hourly_rate: {
+      type: Number,
+    },
+    type: {
+      type: String,
+    },
+    job_ts: {
+      type: Number,
+    },
+    proposals_tier: {
+      type: String,
+    },
+    published_on: { type: Date, default: Date.now },
+    tier: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    uid: {
+      type: Number,
+    },
+    total_freelancers_to_hire: {
+      type: Number,
+    },
+    client_company_org_uid: {
+      type: Number,
+    },
+    client_payment_verification_status: {
+      type: Number,
+    },
+    client_total_feedback: {
+      type: Number,
+    },
+    occupations_category_pref_label: {
+      type: String,
+    },
+    occupations_oservice_pref_label: {
+      type: String,
+    },
+    client_total_reviews: {
+      type: Number,
+    },
+    client_total_spent: {
+      type: Number,
+    },
+    client_location_country: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.model("Gig", GigSchema);
+gigsSchema.plugin(mongooseAggregatePaginate);
+
+export const Gigs = mongoose.model("Gigs", gigsSchema);
