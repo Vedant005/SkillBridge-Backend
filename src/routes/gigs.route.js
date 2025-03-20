5951eebdad595906419695840aaeb7ec1a4e8a99
Vedant005 <vedantkanekar909@gmail.com>
2025-03-20 18:34:06 +0530

    feat: add routes for creating, updating, and deleting gigs

--- src/routes/gigs.route.js
+++ src/routes/gigs.route.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { getGigs } from "../controllers/gigs.controller.js";
+import {
+  createGigs,
+  deleteGigs,
+  getGigs,
+  updateGigs,
+} from "../controllers/gigs.controller.js";
 
 const router = Router();
 
 router.route("/").get(getGigs);
+
+router.route("/create").post(createGigs);
+
+router.route("/update").patch(updateGigs);
+
+router.route("/delete").delete(deleteGigs);
 
 export default router;
